4621853150b5af0ac1886b2c373bb19d413d89ea
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2018-12-22 23:20:29 -0600

    Update in2.asm

--- in2.asm
+++ in2.asm
@@ -3,7 +3,7 @@
 				ld 2 .vals1
 				ld 3 .vals2
 				ldi 5 0
-	.loop	add 5 2
+	.loop			add 5 2
 				add 5 3
 				st  5 .result
 				inc 3
@@ -11,9 +11,9 @@
 				dec 1
 				bnz 1 .loop
 				sys 1 16
-.count	dw  3
-.vals1	dw  1
-.vals2  dw  4
-.result dw  0
-16			dw  0
+	.count			dw  3
+	.vals1			dw  1
+	.vals2  		dw  4
+	.result 		dw  0
+	16			dw  0
 				end
